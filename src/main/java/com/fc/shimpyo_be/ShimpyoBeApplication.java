6eceba4ddeaf055ee27774eee20305d5bf0bd5c3
JeongUijeong <jeong275117@gmail.com>
2023-11-20 19:22:51 +0900

    init: Spring Boot 프로젝트 생성

---
+++ src/main/java/com/fc/shimpyo_be/ShimpyoBeApplication.java
@@ -0,0 +1,13 @@
+package com.fc.shimpyo_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShimpyoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShimpyoBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fc/shimpyo_be/ShimpyoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fc.shimpyo_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShimpyoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
