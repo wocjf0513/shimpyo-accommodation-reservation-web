79ae257e61719043355220d07c4aa6d6d10683cd
JeongUijeong <jeong275117@gmail.com>
2023-11-20 21:58:03 +0900

    #1 feat: 숙박 상품 방 Repository 구현

---
+++ src/main/java/com/fc/shimpyo_be/domain/room/repository/RoomRepository.java
@@ -0,0 +1,8 @@
+package com.fc.shimpyo_be.domain.room.repository;
+
+import com.fc.shimpyo_be.domain.room.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+}
