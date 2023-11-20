3903552ed4b4b89fdf5e376739d7b6060361784c
JeongUijeong <jeong275117@gmail.com>
2023-11-20 20:41:11 +0900

    #1 feat: QueryDSL 정렬을 위한 util 클래스 구현

---
+++ src/main/java/com/fc/shimpyo_be/global/util/QueryDslUtil.java
@@ -0,0 +1,14 @@
+package com.fc.shimpyo_be.global.util;
+
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.Expressions;
+
+public class QueryDslUtil {
+
+    public static OrderSpecifier<?> getSortedColumn(Order order, Path<?> parent, String fieldName) {
+        Path<Object> fieldPath = Expressions.path(Object.class, parent, fieldName);
+        return new OrderSpecifier(order, fieldPath);
+    }
+}
