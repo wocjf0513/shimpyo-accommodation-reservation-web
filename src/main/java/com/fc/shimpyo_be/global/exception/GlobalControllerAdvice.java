18a6d446ba925388f4c01a14e439dd0eab855224
JeongUijeong <jeong275117@gmail.com>
2023-11-20 20:48:18 +0900

    #1 refactor: ResponseDto 클래스 rename에 따른 수정

--- src/main/java/com/fc/shimpyo_be/global/exception/GlobalControllerAdvice.java
+++ src/main/java/com/fc/shimpyo_be/global/exception/GlobalControllerAdvice.java
@@ -1,6 +1,6 @@
 package com.fc.shimpyo_be.global.exception;
 
-import com.fc.shimpyo_be.global.common.ResponseDTO;
+import com.fc.shimpyo_be.global.common.ResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalControllerAdvice {
 
     @ExceptionHandler
-    public ResponseEntity<ResponseDTO<Void>> bindException(BindException e) {
+    public ResponseEntity<ResponseDto<Void>> bindException(BindException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-            ResponseDTO.res(HttpStatus.BAD_REQUEST,
+            ResponseDto.res(HttpStatus.BAD_REQUEST,
                 e.getBindingResult().getAllErrors().get(0).getDefaultMessage()));
     }
 }
