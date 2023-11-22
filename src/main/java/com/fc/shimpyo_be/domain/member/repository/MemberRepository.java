7735fd01d924fdbc701f945c7e09296ac843fc7e
JeongUijeong <jeong275117@gmail.com>
2023-11-23 03:51:48 +0900

    #6 feat: 회원 Repository 메서드 추가
    - 이메일로 회원 정보를 조회하는 메서드
    - 이메일에 해당하는 회원 정보 존재 여부 확인 메서드

--- src/main/java/com/fc/shimpyo_be/domain/member/repository/MemberRepository.java
+++ src/main/java/com/fc/shimpyo_be/domain/member/repository/MemberRepository.java
@@ -1,8 +1,12 @@
 package com.fc.shimpyo_be.domain.member.repository;
 
 import com.fc.shimpyo_be.domain.member.entity.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    Optional<Member> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
