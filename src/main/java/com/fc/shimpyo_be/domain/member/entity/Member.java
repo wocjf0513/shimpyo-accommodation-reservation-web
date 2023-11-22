abc1c089410566cc9770df6a389cf91e0d905d9e
JeongUijeong <jeong275117@gmail.com>
2023-11-23 03:49:34 +0900

    #6 feat: Authority 구현

--- src/main/java/com/fc/shimpyo_be/domain/member/entity/Member.java
+++ src/main/java/com/fc/shimpyo_be/domain/member/entity/Member.java
@@ -3,6 +3,8 @@ package com.fc.shimpyo_be.domain.member.entity;
 import com.fc.shimpyo_be.global.common.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -19,7 +21,7 @@ public class Member extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true, nullable = false, length = 3)
+    @Column(unique = true, nullable = false, length = 30)
     private String email;
     @Column(nullable = false, length = 30)
     private String name;
@@ -27,13 +29,17 @@ public class Member extends BaseTimeEntity {
     private String password;
     @Column(nullable = false, columnDefinition = "TEXT")
     private String photoUrl;
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
 
     @Builder
-    public Member(Long id, String email, String name, String password, String photoUrl) {
+    public Member(Long id, String email, String name, String password, String photoUrl,
+        Authority authority) {
         this.id = id;
         this.email = email;
         this.name = name;
         this.password = password;
         this.photoUrl = photoUrl;
+        this.authority = authority;
     }
 }
