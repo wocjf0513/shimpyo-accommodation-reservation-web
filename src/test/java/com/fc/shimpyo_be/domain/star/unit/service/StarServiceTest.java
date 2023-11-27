48012dbf84c04d1a87a4acc33809f79fec514c36
jo0oy <rkfkrh07@naver.com>
2023-11-27 21:10:14 +0900

    #12 fix: StarServiceTest @SpringBootTest에서 Mockito Test로 수정

--- src/test/java/com/fc/shimpyo_be/domain/star/unit/service/StarServiceTest.java
+++ src/test/java/com/fc/shimpyo_be/domain/star/unit/service/StarServiceTest.java
@@ -10,31 +10,40 @@ import com.fc.shimpyo_be.domain.product.exception.ProductNotFoundException;
 import com.fc.shimpyo_be.domain.product.repository.ProductRepository;
 import com.fc.shimpyo_be.domain.star.dto.request.StarRegisterRequestDto;
 import com.fc.shimpyo_be.domain.star.dto.response.StarResponseDto;
+import com.fc.shimpyo_be.domain.star.entity.Star;
 import com.fc.shimpyo_be.domain.star.repository.StarRepository;
 import com.fc.shimpyo_be.domain.star.service.StarService;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import java.util.Optional;
 
-@SpringBootTest
-class StarServiceTest {
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
-    @Autowired
+
+@ExtendWith(MockitoExtension.class)
+public class StarServiceTest {
+
+    @InjectMocks
     private StarService starService;
 
-    @Autowired
+    @Mock
     private StarRepository starRepository;
 
-    @Autowired
+    @Mock
     private MemberRepository memberRepository;
 
-    @Autowired
+    @Mock
     private ProductRepository productRepository;
 
     private Member member;
@@ -43,42 +52,58 @@ class StarServiceTest {
 
     @BeforeEach
     void setUp() {
-        member = memberRepository.save(
-            Member.builder()
-                .email("member@email.com")
-                .name("member")
-                .password("password")
-                .photoUrl("photoUrl")
-                .authority(Authority.ROLE_USER)
-                .build()
-        );
+        member = Member.builder()
+            .id(1L)
+            .email("member@email.com")
+            .name("member")
+            .password("password")
+            .photoUrl("photoUrl")
+            .authority(Authority.ROLE_USER)
+            .build();
 
-        product = productRepository.save(
-            Product.builder()
-                .name("숙소1")
-                .description("숙소 설명")
-                .category(Category.HOTEL)
-                .thumbnail("thumbnail url")
-                .starAvg(3.5f)
-                .address("숙소 주소")
-                .build()
-        );
+        product = Product.builder()
+            .id(1L)
+            .name("숙소1")
+            .description("숙소 설명")
+            .category(Category.HOTEL)
+            .thumbnail("thumbnail url")
+            .starAvg(3.5f)
+            .address("숙소 주소")
+            .build();
     }
 
     @DisplayName("별점 등록에 성공한다.")
     @Test
-    void register() {
+    void register_test() {
         // given
         float score = 3.5F;
-        long memberId = member.getId();
         StarRegisterRequestDto request
             = new StarRegisterRequestDto(product.getId(), score);
 
+        given(memberRepository.findById(anyLong()))
+            .willReturn(Optional.of(member));
+        given(productRepository.findById(anyLong()))
+            .willReturn(Optional.of(product));
+        given(starRepository.save(any(Star.class)))
+            .willReturn(
+                Star.builder()
+                    .id(1L)
+                    .member(member)
+                    .product(product)
+                    .score(score)
+                    .build()
+            );
+
         // when
-        StarResponseDto result = starService.register(memberId, request);
+        StarResponseDto response = starService.register(member.getId(), request);
 
         // then
-        assertThat(result.score()).isEqualTo(score);
+        assertThat(response).isNotNull();
+        assertThat(response.score()).isEqualTo(score);
+
+        verify(memberRepository, times(1)).findById(anyLong());
+        verify(productRepository, times(1)).findById(anyLong());
+        verify(starRepository, times(1)).save(any(Star.class));
     }
 
     @DisplayName("별점 등록시, 회원 정보가 존재하지 않을 경우 실패한다.")
@@ -90,9 +115,16 @@ class StarServiceTest {
         StarRegisterRequestDto request
             = new StarRegisterRequestDto(product.getId(), score);
 
+        given(memberRepository.findById(anyLong()))
+            .willThrow(MemberNotFoundException.class);
+
         // when & then
         assertThatThrownBy(() -> starService.register(memberId, request))
             .isInstanceOf(MemberNotFoundException.class);
+
+        verify(memberRepository, times(1)).findById(anyLong());
+        verify(productRepository, times(0)).findById(anyLong());
+        verify(starRepository, times(0)).save(any(Star.class));
     }
 
     @DisplayName("별점 등록시, 숙소 정보가 존재하지 않을 경우 실패한다.")
@@ -105,15 +137,17 @@ class StarServiceTest {
         StarRegisterRequestDto request
             = new StarRegisterRequestDto(productId, score);
 
+        given(memberRepository.findById(anyLong()))
+            .willReturn(Optional.ofNullable(member));
+        given(productRepository.findById(anyLong()))
+            .willThrow(ProductNotFoundException.class);
+
         // when & then
         assertThatThrownBy(() -> starService.register(memberId, request))
             .isInstanceOf(ProductNotFoundException.class);
-    }
 
-    @AfterEach
-    void tearDown() {
-        starRepository.deleteAll();
-        memberRepository.deleteAll();
-        productRepository.deleteAll();
+        verify(memberRepository, times(1)).findById(anyLong());
+        verify(productRepository, times(1)).findById(anyLong());
+        verify(starRepository, times(0)).save(any(Star.class));
     }
 }
