3b7218f1b0994439e58917a6e13abd908bf8a59b
jaecheol sim <wocjf0513@ajou.ac.kr>
2023-11-28 16:25:19 +0900

    #43 상품 관련 test 작성 중

--- src/test/java/com/fc/shimpyo_be/domain/product/unit/service/ProductServiceTest.java
+++ src/test/java/com/fc/shimpyo_be/domain/product/unit/service/ProductServiceTest.java
@@ -1,4 +1,4 @@
-package com.fc.shimpyo_be.domain.product.service;
+package com.fc.shimpyo_be.domain.product.unit.service;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
@@ -8,6 +8,7 @@ import com.fc.shimpyo_be.domain.product.entity.Category;
 import com.fc.shimpyo_be.domain.product.entity.Product;
 import com.fc.shimpyo_be.domain.product.repository.ProductRepository;
 import com.fc.shimpyo_be.domain.product.repository.model.ProductSpecification;
+import com.fc.shimpyo_be.domain.product.service.ProductService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
