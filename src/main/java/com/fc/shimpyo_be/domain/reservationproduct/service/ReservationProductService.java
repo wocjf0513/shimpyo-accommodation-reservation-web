3893d7be324aa91babfbfb96901fe41b673f8f77
jo0oy <rkfkrh07@naver.com>
2023-11-30 03:58:31 +0900

    #53 feat: 예약 주문 상품 취소 기능 서비스 비즈니스 로직 구현
    - Reservation 엔티티 필요하기 때문에 ReservationProductRepository 에 Reservation을 함께 페치조인 해오는 JPQL 구현

---
+++ src/main/java/com/fc/shimpyo_be/domain/reservationproduct/service/ReservationProductService.java
@@ -0,0 +1,37 @@
+package com.fc.shimpyo_be.domain.reservationproduct.service;
+
+import com.fc.shimpyo_be.domain.reservation.entity.Reservation;
+import com.fc.shimpyo_be.domain.reservationproduct.entity.ReservationProduct;
+import com.fc.shimpyo_be.domain.reservationproduct.exception.ForbiddenCancelReservationProductException;
+import com.fc.shimpyo_be.domain.reservationproduct.exception.ReservationProductNotFoundException;
+import com.fc.shimpyo_be.domain.reservationproduct.repository.ReservationProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class ReservationProductService {
+
+    private final ReservationProductRepository reservationProductRepository;
+
+    @Transactional
+    public void cancel(Long id, Long memberId) {
+        log.info("{} ::: {}", getClass().getSimpleName(), "cancel");
+
+        ReservationProduct reservationProduct = reservationProductRepository.findByIdWithReservation(id)
+            .orElseThrow(ReservationProductNotFoundException::new);
+
+        Reservation reservation = reservationProduct.getReservation();
+
+        // 취소할 권한 확인 후, 에러 처리
+        if(!reservation.getMember().getId().equals(memberId)) {
+            throw new ForbiddenCancelReservationProductException();
+        }
+
+        reservationProduct.cancel();
+        reservation.minusPrice(reservationProduct.getPrice());
+    }
+}
