093a26cd6d2c913780e9918c737324f837568c37
jo0oy <rkfkrh07@naver.com>
2023-11-30 03:59:06 +0900

    #53 feat: 예약 주문 상품 취소 API 구현
    - DELETE /api/reservation-products/{id}

---
+++ src/main/java/com/fc/shimpyo_be/domain/reservationproduct/controller/ReservationProductRestController.java
@@ -0,0 +1,34 @@
+package com.fc.shimpyo_be.domain.reservationproduct.controller;
+
+import com.fc.shimpyo_be.domain.reservationproduct.service.ReservationProductService;
+import com.fc.shimpyo_be.global.common.ResponseDto;
+import com.fc.shimpyo_be.global.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/reservation-products")
+@RestController
+public class ReservationProductRestController {
+
+    private final ReservationProductService reservationProductService;
+    private final SecurityUtil securityUtil;
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResponseDto<Void>> cancel(@PathVariable Long id) {
+        log.info("[api][DELETE] /api/reservation-products");
+
+        reservationProductService.cancel(id, securityUtil.getCurrentMemberId());
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(ResponseDto.res(HttpStatus.OK, "예약 상품이 정상적으로 취소 처리되었습니다."));
+    }
+}
