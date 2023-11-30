a37187dfee298bc8c5b9d1329afecc5d73c7b247
JeongUijeong <jeong275117@gmail.com>
2023-11-30 17:59:56 +0900

    #65 feat: 회원 컨트롤러에 로깅 추가

--- src/main/java/com/fc/shimpyo_be/domain/member/controller/MemberRestController.java
+++ src/main/java/com/fc/shimpyo_be/domain/member/controller/MemberRestController.java
@@ -5,7 +5,9 @@ import com.fc.shimpyo_be.domain.member.dto.request.UpdateMemberRequestDto;
 import com.fc.shimpyo_be.domain.member.dto.response.MemberResponseDto;
 import com.fc.shimpyo_be.domain.member.service.MemberService;
 import com.fc.shimpyo_be.global.common.ResponseDto;
+import com.fc.shimpyo_be.global.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,15 +17,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/members")
 public class MemberRestController {
 
     private final MemberService memberService;
+    private final SecurityUtil securityUtil;
 
     @GetMapping
     public ResponseEntity<ResponseDto<MemberResponseDto>> getMember() {
+        log.debug("memberId: {}", securityUtil.getCurrentMemberId());
         return ResponseEntity.status(HttpStatus.OK).body(
             ResponseDto.res(HttpStatus.OK, memberService.getMember(),
                 "성공적으로 회원 정보를 조회했습니다."));
@@ -32,6 +37,7 @@ public class MemberRestController {
     @PatchMapping
     public ResponseEntity<ResponseDto<MemberResponseDto>> updateMember(@RequestBody
     UpdateMemberRequestDto updateMemberRequestDto) {
+        log.debug("memberId: {}", securityUtil.getCurrentMemberId());
         return ResponseEntity.status(HttpStatus.OK).body(
             ResponseDto.res(HttpStatus.OK, memberService.updateMember(updateMemberRequestDto),
                 "성공적으로 회원 정보를 수정했습니다."));
@@ -40,6 +46,7 @@ public class MemberRestController {
     @PostMapping
     public ResponseEntity<ResponseDto<Void>> checkPassword(
         @RequestBody CheckPasswordRequestDto checkPasswordRequestDto) {
+        log.debug("memberId: {}", securityUtil.getCurrentMemberId());
         memberService.checkPassword(checkPasswordRequestDto);
         return ResponseEntity.status(HttpStatus.OK).body(
             ResponseDto.res(HttpStatus.OK, "비밀번호가 일치합니다."));
