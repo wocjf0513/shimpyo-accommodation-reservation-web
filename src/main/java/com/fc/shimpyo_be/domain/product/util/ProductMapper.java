d8582e5321aa4f592eaf8649bc4899f36a8e95b4
JeongUijeong <jeong275117@gmail.com>
2023-12-07 01:14:26 +0900

    #87 refactor: 객실 가격 엔터티 분리에 따라 로직 임시 수정
    - 날짜에 딸라 가격이 달라지므로 로직 수정이 필요

--- src/main/java/com/fc/shimpyo_be/domain/product/util/ProductMapper.java
+++ src/main/java/com/fc/shimpyo_be/domain/product/util/ProductMapper.java
@@ -5,6 +5,7 @@ import com.fc.shimpyo_be.domain.product.dto.response.ProductResponse;
 import com.fc.shimpyo_be.domain.product.entity.Product;
 import com.fc.shimpyo_be.domain.product.entity.ProductImage;
 import com.fc.shimpyo_be.domain.room.entity.Room;
+import com.fc.shimpyo_be.domain.room.entity.RoomPrice;
 import com.fc.shimpyo_be.domain.room.util.RoomMapper;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,12 +14,15 @@ public class ProductMapper {
 
     public static ProductResponse toProductResponse(Product product) {
         return ProductResponse.builder().productId(product.getId()).productName(product.getName())
-            .address(product.getAddress()).category(product.getCategory().getName())
+            .address(
+                product.getAddress().getAddress() + " " + product.getAddress().getDetailAddress())
+            .category(product.getCategory().getName())
             .image(product.getThumbnail())
             .starAvg(product.getStarAvg())
             .price(product.getRooms().isEmpty()
                 ? 0 : Long.valueOf(
-                product.getRooms().stream().map(Room::getPrice).min((o1, o2) -> o1 - o2)
+                    // TODO 해당 숙소 객실 가격 중 가장 낮은 가격을 반환해야 하지만, 날짜에 따라 가격이 달라지므로 로직 수정이 필요함
+                product.getRooms().stream().map(Room::getPrice).map(RoomPrice::getPeakWeekendMinFee).min((o1, o2) -> o1 - o2)
                     .orElseThrow()))
             .capacity(product.getRooms().isEmpty()
                 ? 0 : Long.valueOf(
@@ -37,7 +41,8 @@ public class ProductMapper {
         return ProductDetailsResponse.builder()
             .productId(product.getId())
             .category(product.getCategory().getName())
-            .address(product.getAddress())
+            .address(
+                product.getAddress().getAddress() + " " + product.getAddress().getDetailAddress())
             .productName(product.getName())
             .description(product.getDescription())
             .starAvg(product.getStarAvg())
