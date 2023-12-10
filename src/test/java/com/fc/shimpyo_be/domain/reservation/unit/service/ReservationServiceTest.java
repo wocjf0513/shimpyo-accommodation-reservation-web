3a5a98745848e9b3ba6a8aaaecbcf8225b5905ad
jo0oy <rkfkrh07@naver.com>
2023-12-11 03:48:28 +0900

    #97 test: ReservationServiceTest에 @Sql 제거
    - @Sql 모두 제거하고 @BeforeEach로 필요한 테스트 데이터 생성
    - DatabaseCleanUp 사용하기 위해 @Import(TestDBCleanerConfig.class) 추가

--- src/test/java/com/fc/shimpyo_be/domain/reservation/unit/service/ReservationServiceTest.java
+++ src/test/java/com/fc/shimpyo_be/domain/reservation/unit/service/ReservationServiceTest.java
@@ -1,56 +1,182 @@
 package com.fc.shimpyo_be.domain.reservation.unit.service;
 
 import com.fc.shimpyo_be.config.AbstractContainersSupport;
+import com.fc.shimpyo_be.config.DatabaseCleanUp;
+import com.fc.shimpyo_be.config.TestDBCleanerConfig;
+import com.fc.shimpyo_be.domain.member.entity.Authority;
+import com.fc.shimpyo_be.domain.member.entity.Member;
 import com.fc.shimpyo_be.domain.member.exception.MemberNotFoundException;
+import com.fc.shimpyo_be.domain.member.repository.MemberRepository;
+import com.fc.shimpyo_be.domain.product.entity.*;
 import com.fc.shimpyo_be.domain.product.exception.RoomNotFoundException;
+import com.fc.shimpyo_be.domain.product.repository.ProductRepository;
 import com.fc.shimpyo_be.domain.reservation.dto.request.SaveReservationRequestDto;
 import com.fc.shimpyo_be.domain.reservation.dto.response.SaveReservationResponseDto;
 import com.fc.shimpyo_be.domain.reservation.entity.PayMethod;
 import com.fc.shimpyo_be.domain.reservation.service.ReservationService;
 import com.fc.shimpyo_be.domain.reservationproduct.dto.request.ReservationProductRequestDto;
-import lombok.extern.slf4j.Slf4j;
-import com.fc.shimpyo_be.domain.reservationproduct.repository.ReservationProductRepository;
 import com.fc.shimpyo_be.domain.room.entity.Room;
+import com.fc.shimpyo_be.domain.room.entity.RoomOption;
 import com.fc.shimpyo_be.domain.room.entity.RoomPrice;
 import com.fc.shimpyo_be.domain.room.repository.RoomRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
+import org.springframework.context.annotation.Import;
 
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
+@Import(TestDBCleanerConfig.class)
 @SpringBootTest
 public class ReservationServiceTest extends AbstractContainersSupport {
 
     @Autowired
     private ReservationService reservationService;
 
-    @Sql("classpath:testdata/reservation-service-setup.sql")
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Autowired
+    private DatabaseCleanUp databaseCleanUp;
+
+    private Member member;
+
+    private final String[] tableNameArray = {
+        "member", "product", "room", "product_option", "address", "room_option", "amenity"
+    };
+
+    @BeforeEach
+    void setUp() {
+        databaseCleanUp.cleanUp(tableNameArray);
+
+        member = memberRepository.save(
+            Member.builder()
+                .email("member@email.com")
+                .name("member")
+                .password("password")
+                .photoUrl("member photo url")
+                .authority(Authority.ROLE_USER)
+                .build()
+        );
+
+        List<Product> products = new ArrayList<>();
+        for (int i = 1; i <= 3; i++) {
+            String productName = "호텔" + i;
+            float starAvg = ThreadLocalRandom.current().nextFloat(0, 5);
+            String infoCenter = String.format("02-1234-%d%d%d%d", i, i, i, i);
+            products.add(
+                productRepository.save(
+                    Product.builder()
+                        .name(productName)
+                        .thumbnail(productName + " 썸네일 url")
+                        .description(productName + " 설명")
+                        .starAvg(starAvg)
+                        .category(Category.TOURIST_HOTEL)
+                        .address(
+                            Address.builder()
+                                .address(productName + " 주소")
+                                .detailAddress(productName + " 상세 주소")
+                                .mapX(1.0)
+                                .mapY(1.5)
+                                .build()
+                        )
+                        .productOption(
+                            ProductOption.builder()
+                                .cooking(true)
+                                .foodPlace("음료 가능")
+                                .parking(true)
+                                .pickup(false)
+                                .infoCenter(infoCenter)
+                                .build()
+                        )
+                        .amenity(
+                            Amenity.builder()
+                                .barbecue(false)
+                                .beauty(true)
+                                .beverage(true)
+                                .fitness(true)
+                                .bicycle(false)
+                                .campfire(false)
+                                .karaoke(true)
+                                .publicBath(true)
+                                .publicPc(true)
+                                .seminar(false)
+                                .sports(false)
+                                .build()
+                        )
+                        .build()
+                )
+            );
+        }
+
+        List<Room> rooms = new ArrayList<>();
+        for (int i = 1; i <= 3; i++) {
+            String roomName = "객실" + i;
+            rooms.add(
+                roomRepository.save(
+                    Room.builder()
+                        .product(products.get(i - 1))
+                        .name(roomName)
+                        .description(roomName + " 설명")
+                        .standard(2)
+                        .capacity(4)
+                        .checkIn(LocalTime.of(14, 0))
+                        .checkOut(LocalTime.of(12, 0))
+                        .price(
+                            RoomPrice.builder()
+                                .offWeekDaysMinFee(75000)
+                                .offWeekendMinFee(85000)
+                                .peakWeekDaysMinFee(100000)
+                                .peakWeekendMinFee(120000)
+                                .build()
+                        )
+                        .roomOption(
+                            RoomOption.builder()
+                                .cooking(true)
+                                .airCondition(true)
+                                .bath(true)
+                                .bathFacility(true)
+                                .pc(false)
+                                .diningTable(true)
+                                .hairDryer(true)
+                                .homeTheater(false)
+                                .internet(true)
+                                .cable(false)
+                                .refrigerator(true)
+                                .sofa(true)
+                                .toiletries(true)
+                                .tv(true)
+                                .build()
+                        )
+                        .build()
+                )
+            );
+        }
+    }
+
     @DisplayName("정상적으로 예약을 저장할 수 있다.")
     @Test
     void saveReservation_test() {
         //given
-        long memberId = 1L;
+        long memberId = member.getId();
         long roomId1 = 1L;
         long roomId2 = 2L;
-
-        Room room = Room.builder()
-            .id(1L)
-            .name("room1")
-            .description("description")
-            .price(RoomPrice.builder()
-                .offWeekDaysMinFee(50000)
-                .offWeekendMinFee(60000)
-                .peakWeekDaysMinFee(100000)
-                .peakWeekendMinFee(110000)
-                .build())
-            .build();
 
         SaveReservationRequestDto requestDto
             = new SaveReservationRequestDto(
@@ -102,7 +228,7 @@ public class ReservationServiceTest extends AbstractContainersSupport {
     @Test
     void saveReservation_roomNotFound_test() {
         //given
-        long memberId = 1L;
+        long memberId = member.getId();
         long roomId1 = 1L;
         long roomId2 = 2000L;
 
