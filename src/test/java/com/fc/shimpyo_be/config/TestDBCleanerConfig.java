03fc1c901fd691911b34c47289a76ca6ec2f5843
jo0oy <rkfkrh07@naver.com>
2023-12-11 03:34:35 +0900

    #97 test: TestDBCleanerConfig를 테스트전용 설정파일로 변경
    - @Configuration to @TestConfiguration

--- src/test/java/com/fc/shimpyo_be/config/TestDBCleanerConfig.java
+++ src/test/java/com/fc/shimpyo_be/config/TestDBCleanerConfig.java
@@ -1,9 +1,9 @@
 package com.fc.shimpyo_be.config;
 
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@TestConfiguration
 public class TestDBCleanerConfig {
 
     @Bean
