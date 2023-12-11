245b16c37ca7b2235577d53154f4f7b4c4b5acb6
jo0oy <rkfkrh07@naver.com>
2023-12-12 05:18:11 +0900

    #92 refactor: 관련 DTO에 @Builder 추가 및 RoomWithProductResponseDto에 'productDetailAddress' 추가

--- src/main/java/com/fc/shimpyo_be/domain/room/dto/response/RoomListWithProductInfoResponseDto.java
+++ src/main/java/com/fc/shimpyo_be/domain/room/dto/response/RoomListWithProductInfoResponseDto.java
@@ -1,7 +1,10 @@
 package com.fc.shimpyo_be.domain.room.dto.response;
 
+import lombok.Builder;
+
 import java.util.List;
 
+@Builder
 public record RoomListWithProductInfoResponseDto(
     List<RoomWithProductResponseDto> rooms
 ) {
--- src/main/java/com/fc/shimpyo_be/domain/room/dto/response/RoomWithProductResponseDto.java
+++ src/main/java/com/fc/shimpyo_be/domain/room/dto/response/RoomWithProductResponseDto.java
@@ -1,16 +1,20 @@
 package com.fc.shimpyo_be.domain.room.dto.response;
 
+import lombok.Builder;
+
+@Builder
 public record RoomWithProductResponseDto(
     Long productId,
     String productName,
     String productThumbnail,
     String productAddress,
+    String productDetailAddress,
     Long roomId,
     String roomName,
     Integer standard,
     Integer capacity,
     String checkIn,
     String checkOut,
-    Integer price
+    Long price
 ) {
 }
