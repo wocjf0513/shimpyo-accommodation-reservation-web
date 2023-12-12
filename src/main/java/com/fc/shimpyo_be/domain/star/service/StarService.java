0f644498c7b3db4f873be3c867ca0e6d099072c2
jo0oy <rkfkrh07@naver.com>
2023-12-13 01:50:12 +0900

    #100 feat: 체크아웃 2주 이후일 경우 별점 등록 불가 검증 로직 구현

--- src/main/java/com/fc/shimpyo_be/domain/star/service/StarService.java
+++ src/main/java/com/fc/shimpyo_be/domain/star/service/StarService.java
@@ -1,8 +1,7 @@
 package com.fc.shimpyo_be.domain.star.service;
 
 import com.fc.shimpyo_be.domain.member.entity.Member;
-import com.fc.shimpyo_be.domain.member.exception.MemberNotFoundException;
-import com.fc.shimpyo_be.domain.member.repository.MemberRepository;
+import com.fc.shimpyo_be.domain.member.service.MemberService;
 import com.fc.shimpyo_be.domain.product.entity.Product;
 import com.fc.shimpyo_be.domain.product.exception.ProductNotFoundException;
 import com.fc.shimpyo_be.domain.product.repository.ProductRepository;
@@ -13,6 +12,7 @@ import com.fc.shimpyo_be.domain.star.dto.request.StarRegisterRequestDto;
 import com.fc.shimpyo_be.domain.star.dto.response.StarResponseDto;
 import com.fc.shimpyo_be.domain.star.entity.Star;
 import com.fc.shimpyo_be.domain.star.exception.CannotBeforeCheckOutException;
+import com.fc.shimpyo_be.domain.star.exception.ExpiredRegisterDateException;
 import com.fc.shimpyo_be.domain.star.repository.StarRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,7 +29,7 @@ import java.time.LocalTime;
 public class StarService {
 
     private final StarRepository starRepository;
-    private final MemberRepository memberRepository;
+    private final MemberService memberService;
     private final ProductRepository productRepository;
     private final ReservationProductRepository reservationProductRepository;
 
@@ -38,8 +38,7 @@ public class StarService {
         log.debug("{} ::: {}", getClass().getSimpleName(), "register");
 
         // 회원 조회
-        Member member = memberRepository.findById(memberId)
-            .orElseThrow(MemberNotFoundException::new);
+        Member member = memberService.getMemberById(memberId);
 
         // 예약 숙소 조회
         ReservationProduct reservationProduct = reservationProductRepository.findByIdWithRoom(request.reservationProductId())
@@ -77,11 +76,19 @@ public class StarService {
     }
 
     private void validateRegisterDate(LocalDateTime current, LocalDate endDate, LocalTime checkOut) {
-        LocalDateTime target = LocalDateTime.of(endDate, checkOut);
+        LocalDateTime startDateTime = LocalDateTime.of(endDate, checkOut);
 
-        if (current.isEqual(target) || current.isBefore(target)) {
+        if (current.isEqual(startDateTime) || current.isBefore(startDateTime)) {
             log.error("별점 등록은 체크아웃 이후에 가능합니다.");
             throw new CannotBeforeCheckOutException();
+        }
+
+        LocalDateTime endDateTime =
+            LocalDateTime.of(endDate.plusDays(14), LocalTime.of(23, 59, 59));
+
+        if (current.isAfter(endDateTime)) {
+            log.error("별점 등록은 체크아웃 후 2주 이내에만 가능합니다.");
+            throw new ExpiredRegisterDateException();
         }
     }
 }
