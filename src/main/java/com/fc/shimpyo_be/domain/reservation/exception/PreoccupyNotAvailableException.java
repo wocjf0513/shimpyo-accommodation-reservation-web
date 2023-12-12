c8d21f02003230481e22ee1361bc50171dc88a0d
jo0oy <rkfkrh07@naver.com>
2023-12-12 21:40:49 +0900

    #109 refactor, rename: 각 선점 불가, 예약 불가 Custom Exception 수정 및 클래스명 변경

--- src/main/java/com/fc/shimpyo_be/domain/reservation/exception/PreoccupyNotAvailableException.java
+++ src/main/java/com/fc/shimpyo_be/domain/reservation/exception/PreoccupyNotAvailableException.java
@@ -1,21 +1,21 @@
 package com.fc.shimpyo_be.domain.reservation.exception;
 
-import com.fc.shimpyo_be.domain.reservation.dto.response.ValidationResultResponseDto;
+import com.fc.shimpyo_be.domain.reservation.dto.response.ValidatePreoccupyResultResponseDto;
 import com.fc.shimpyo_be.global.exception.ErrorCode;
 import lombok.Getter;
 
 @Getter
-public class UnavailableRoomsException extends RuntimeException {
+public class PreoccupyNotAvailableException extends RuntimeException {
 
     private final ErrorCode errorCode;
-    private ValidationResultResponseDto data;
+    private ValidatePreoccupyResultResponseDto data;
 
-    public UnavailableRoomsException() {
+    public PreoccupyNotAvailableException() {
         super(ErrorCode.UNAVAILABLE_ROOMS.getSimpleMessage());
         this.errorCode = ErrorCode.UNAVAILABLE_ROOMS;
     }
 
-    public UnavailableRoomsException(ValidationResultResponseDto data) {
+    public PreoccupyNotAvailableException(ValidatePreoccupyResultResponseDto data) {
         super(ErrorCode.UNAVAILABLE_ROOMS.getSimpleMessage());
         this.errorCode = ErrorCode.UNAVAILABLE_ROOMS;
         this.data = data;
