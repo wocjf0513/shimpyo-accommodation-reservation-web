aa30b7baacf63bf13ec9e03cf7044b55a8c7ac49
JeongUijeong <jeong275117@gmail.com>
2023-12-12 18:44:20 +0900

    #113 feat: 즐겨찾기 조회 기능 구현

--- src/main/java/com/fc/shimpyo_be/domain/favorite/service/FavoriteService.java
+++ src/main/java/com/fc/shimpyo_be/domain/favorite/service/FavoriteService.java
@@ -6,9 +6,13 @@ import com.fc.shimpyo_be.domain.favorite.exception.FavoriteAlreadyRegisterExcept
 import com.fc.shimpyo_be.domain.favorite.repository.FavoriteRepository;
 import com.fc.shimpyo_be.domain.member.entity.Member;
 import com.fc.shimpyo_be.domain.member.service.MemberService;
+import com.fc.shimpyo_be.domain.product.dto.response.ProductResponse;
 import com.fc.shimpyo_be.domain.product.entity.Product;
 import com.fc.shimpyo_be.domain.product.exception.ProductNotFoundException;
 import com.fc.shimpyo_be.domain.product.repository.ProductRepository;
+import com.fc.shimpyo_be.domain.product.util.ProductMapper;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -33,5 +37,15 @@ public class FavoriteService {
             .member(member)
             .product(product)
             .build()));
+    }
+
+    public List<ProductResponse> getFavorites(long memberId) {
+        List<ProductResponse> productResponses = new ArrayList<>();
+        Member member = memberService.getMemberById(memberId);
+        List<Favorite> favorites = favoriteRepository.findByMember(member);
+        for (Favorite favorite : favorites) {
+            productResponses.add(ProductMapper.toProductResponse(favorite.getProduct()));
+        }
+        return productResponses;
     }
 }
