34fc8f3f10e4538f3db997b8e67a22fd5c976f4e
jo0oy <rkfkrh07@naver.com>
2023-12-13 04:10:30 +0900

    #118 test: 숙소 정보 포함 객실 정보 리스트 조회 API 변경에 따라 기존 RoomRestController Test 수정 및 추가

--- src/test/java/com/fc/shimpyo_be/domain/room/unit/controller/RoomRestControllerTest.java
+++ src/test/java/com/fc/shimpyo_be/domain/room/unit/controller/RoomRestControllerTest.java
@@ -1,8 +1,6 @@
 package com.fc.shimpyo_be.domain.room.unit.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fc.shimpyo_be.config.AbstractContainersSupport;
-import com.fc.shimpyo_be.domain.room.dto.request.GetRoomListWithProductInfoRequestDto;
 import com.fc.shimpyo_be.domain.room.dto.response.RoomWithProductResponseDto;
 import com.fc.shimpyo_be.domain.room.service.RoomService;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -21,6 +18,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -38,8 +36,6 @@ public class RoomRestControllerTest extends AbstractContainersSupport {
     @MockBean
     private RoomService roomService;
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
     @BeforeEach
     void setUp(@Autowired WebApplicationContext applicationContext) {
         this.mockMvc = MockMvcBuilders
@@ -50,13 +46,13 @@ public class RoomRestControllerTest extends AbstractContainersSupport {
     }
 
     @WithMockUser(roles = "USER")
-    @DisplayName("[api][GET][정상] 숙소 정보 포함 객실 정보 리스트 조회 API 테스트")
+    @DisplayName("[api][GET][정상] 숙소 정보 포함 객실 정보 리스트 조회 API 테스트 - 콤마 리스트 파라메터")
     @Test
     void getRoomsWithProductInfo_api_test() throws Exception {
         //given
         String requestUrl = "/api/rooms";
-        List<Long> roomIds = List.of(1L, 3L, 4L);
-        GetRoomListWithProductInfoRequestDto requestDto = new GetRoomListWithProductInfoRequestDto(roomIds);
+        String roomIdParamList = "1, 2, 3";
+        List<Long> roomIds = List.of(1L, 2L, 3L);
 
         List<RoomWithProductResponseDto> rooms = List.of(
             RoomWithProductResponseDto.builder()
@@ -109,8 +105,7 @@ public class RoomRestControllerTest extends AbstractContainersSupport {
         //when & then
         mockMvc.perform(
                 get(requestUrl)
-                    .content(objectMapper.writeValueAsString(requestDto))
-                    .contentType(MediaType.APPLICATION_JSON)
+                    .param("roomIds", roomIdParamList)
                     .characterEncoding(StandardCharsets.UTF_8)
             )
             .andExpect(status().isOk())
@@ -121,24 +116,115 @@ public class RoomRestControllerTest extends AbstractContainersSupport {
     }
 
     @WithMockUser(roles = "USER")
-    @DisplayName("[api][GET][실패] 숙소 정보 포함 객실 정보 리스트 조회 API 테스트 - 요청 데이터 검증 에러")
+    @DisplayName("[api][GET][정상] 숙소 정보 포함 객실 정보 리스트 조회 API 테스트 - 파라메터 개별로 매핑")
     @Test
-    void getRoomsWithProductInfo_api_request_validation_fail_test() throws Exception {
+    void getRoomsWithProductInfo_api_request_params_test() throws Exception {
         //given
         String requestUrl = "/api/rooms";
-        List<Long> roomIds = List.of(1L, 3L, 4L, 5L);
-        GetRoomListWithProductInfoRequestDto requestDto = new GetRoomListWithProductInfoRequestDto(roomIds);
+        List<Long> roomIds = List.of(1L, 2L, 3L);
+
+        List<RoomWithProductResponseDto> rooms = List.of(
+            RoomWithProductResponseDto.builder()
+                .productId(1L)
+                .productName("호텔1")
+                .productThumbnail("호텔1 썸네일")
+                .productAddress("호텔1 주소")
+                .productDetailAddress("호텔1 상세 주소")
+                .roomId(1L)
+                .roomName("객실1")
+                .standard(2)
+                .capacity(4)
+                .checkIn("14:00")
+                .checkOut("12:00")
+                .price(80000L)
+                .build(),
+            RoomWithProductResponseDto.builder()
+                .productId(2L)
+                .productName("호텔2")
+                .productThumbnail("호텔2 썸네일")
+                .productAddress("호텔2 주소")
+                .productDetailAddress("호텔2 상세 주소")
+                .roomId(3L)
+                .roomName("객실3")
+                .standard(2)
+                .capacity(4)
+                .checkIn("14:00")
+                .checkOut("11:30")
+                .price(95000L)
+                .build(),
+            RoomWithProductResponseDto.builder()
+                .productId(3L)
+                .productName("호텔3")
+                .productThumbnail("호텔3 썸네일")
+                .productAddress("호텔3 주소")
+                .productDetailAddress("호텔3 상세 주소")
+                .roomId(4L)
+                .roomName("객실4")
+                .standard(2)
+                .capacity(4)
+                .checkIn("13:00")
+                .checkOut("11:00")
+                .price(95000L)
+                .build()
+        );
+
+        given(roomService.getRoomsWithProductInfo(roomIds))
+            .willReturn(rooms);
 
         //when & then
         mockMvc.perform(
                 get(requestUrl)
-                    .content(objectMapper.writeValueAsString(requestDto))
-                    .contentType(MediaType.APPLICATION_JSON)
+                    .param("roomIds", String.valueOf(roomIds.get(0)))
+                    .param("roomIds", String.valueOf(roomIds.get(1)))
+                    .param("roomIds", String.valueOf(roomIds.get(2)))
+                    .characterEncoding(StandardCharsets.UTF_8)
+            )
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.code", is(200)))
+            .andExpect(jsonPath("$.data").isNotEmpty());
+
+        verify(roomService, times(1)).getRoomsWithProductInfo(roomIds);
+    }
+
+    @WithMockUser(roles = "USER")
+    @DisplayName("[api][GET][실패] 숙소 정보 포함 객실 정보 리스트 조회 API 테스트 - 요청 데이터 검증 에러(Size)")
+    @Test
+    void getRoomsWithProductInfo_api_size_validation_fail_test() throws Exception {
+        //given
+        String requestUrl = "/api/rooms";
+        String roomIdParamList = "1, 3, 4, 5";
+        List<Long> roomIds = List.of(1L, 3L, 4L, 5L);
+
+
+        //when & then
+        mockMvc.perform(
+                get(requestUrl)
+                    .param("roomIds", roomIdParamList)
                     .characterEncoding(StandardCharsets.UTF_8)
             )
             .andExpect(status().is4xxClientError())
             .andExpect(jsonPath("$.code", is(400)));
 
         verify(roomService, times(0)).getRoomsWithProductInfo(roomIds);
+    }
+
+    @WithMockUser(roles = "USER")
+    @DisplayName("[api][GET][실패] 숙소 정보 포함 객실 정보 리스트 조회 API 테스트 - 요청 데이터 검증 에러(Min)")
+    @Test
+    void getRoomsWithProductInfo_api_min_validation_fail_test() throws Exception {
+        //given
+        String requestUrl = "/api/rooms";
+        String roomIdParamList = "-2, 3";
+
+        //when & then
+        mockMvc.perform(
+                get(requestUrl)
+                    .param("roomIds", roomIdParamList)
+                    .characterEncoding(StandardCharsets.UTF_8)
+            )
+            .andExpect(status().is4xxClientError())
+            .andExpect(jsonPath("$.code", is(400)));
+
+        verify(roomService, times(0)).getRoomsWithProductInfo(anyList());
     }
 }
