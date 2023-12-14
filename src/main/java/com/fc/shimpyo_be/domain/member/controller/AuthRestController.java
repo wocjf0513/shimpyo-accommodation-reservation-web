f8f01af1ac884dab975828eaeedbb9e1e3fd10e6
JeongUijeong <jeong275117@gmail.com>
2023-12-14 23:45:36 +0900

    refactor: 불필요한 debug 로깅 삭제

--- src/main/java/com/fc/shimpyo_be/domain/member/controller/AuthRestController.java
+++ src/main/java/com/fc/shimpyo_be/domain/member/controller/AuthRestController.java
@@ -9,7 +9,6 @@ import com.fc.shimpyo_be.domain.member.service.AuthService;
 import com.fc.shimpyo_be.global.common.ResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
@@ -28,7 +26,6 @@ public class AuthRestController {
     @PostMapping("/signup")
     public ResponseEntity<ResponseDto<MemberResponseDto>> signUp(
         @Valid @RequestBody SignUpRequestDto signUpRequestDto) {
-        log.debug("email: {}, name: {}", signUpRequestDto.getEmail(), signUpRequestDto.getName());
         return ResponseEntity.status(HttpStatus.CREATED).body(
             ResponseDto.res(HttpStatus.CREATED, authService.signUp(signUpRequestDto),
                 "성공적으로 회원가입 했습니다."));
@@ -37,7 +34,6 @@ public class AuthRestController {
     @PostMapping("/signin")
     public ResponseEntity<ResponseDto<SignInResponseDto>> signIn(
         @Valid @RequestBody SignInRequestDto signInRequestDto) {
-        log.debug("email: {}", signInRequestDto.getEmail());
         return ResponseEntity.status(HttpStatus.OK).body(
             ResponseDto.res(HttpStatus.OK, authService.signIn(signInRequestDto),
                 "성공적으로 로그인 했습니다."));
@@ -46,8 +42,6 @@ public class AuthRestController {
     @PostMapping("/refresh")
     public ResponseEntity<ResponseDto<SignInResponseDto>> refresh(
         @Valid @RequestBody RefreshRequestDto refreshRequestDto) {
-        log.debug("accessToken: {}, refreshToken: {}", refreshRequestDto.getAccessToken(),
-            refreshRequestDto.getRefreshToken());
         return ResponseEntity.status(HttpStatus.OK).body(
             ResponseDto.res(HttpStatus.OK, authService.refresh(refreshRequestDto),
                 "성공적으로 토큰을 재발급 했습니다."));
