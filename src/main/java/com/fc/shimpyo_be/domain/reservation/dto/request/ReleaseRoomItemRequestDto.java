4372274dcb84a2f5399588e155b4f8b26fe49ca5
jo0oy <rkfkrh07@naver.com>
2023-12-14 23:54:05 +0900

    refactor: 예약 도메인 Request DTO 유효성 검증 조건 상수화한 것 적용

--- src/main/java/com/fc/shimpyo_be/domain/reservation/dto/request/ReleaseRoomItemRequestDto.java
+++ src/main/java/com/fc/shimpyo_be/domain/reservation/dto/request/ReleaseRoomItemRequestDto.java
@@ -4,13 +4,15 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 
+import static com.fc.shimpyo_be.domain.reservation.util.constant.ReservationValidationConstants.*;
+
 @Builder
 public record ReleaseRoomItemRequestDto(
     @NotNull
     Long roomId,
-    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "올바른 날짜 형식이 아닙니다.(yyyy-MM-dd 형식으로 입력하세요.)")
+    @Pattern(regexp = DATE_REGEX, message = "올바른 날짜 형식이 아닙니다.(yyyy-MM-dd 형식으로 입력하세요.)")
     String startDate,
-    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "올바른 날짜 형식이 아닙니다.(yyyy-MM-dd 형식으로 입력하세요.)")
+    @Pattern(regexp = DATE_REGEX, message = "올바른 날짜 형식이 아닙니다.(yyyy-MM-dd 형식으로 입력하세요.)")
     String endDate
 ) {
 }
--- src/main/java/com/fc/shimpyo_be/domain/reservationproduct/dto/request/ReservationProductRequestDto.java
+++ src/main/java/com/fc/shimpyo_be/domain/reservationproduct/dto/request/ReservationProductRequestDto.java
@@ -1,6 +1,7 @@
 package com.fc.shimpyo_be.domain.reservationproduct.dto.request;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
@@ -15,7 +16,10 @@ public record ReservationProductRequestDto(
     String startDate,
     @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "올바른 날짜 형식이 아닙니다.(yyyy-MM-dd 형식으로 입력하세요.)")
     String endDate,
+    @NotBlank
     String visitorName,
+    @NotBlank
+        @Pattern(regexp = "^0\\d{1,2}-\\d{3,4}-\\d{4}$")
     String visitorPhone,
     @Min(value = 0, message = "객실 이용 금액은 0원 이상부터 가능합니다.")
     Integer price
